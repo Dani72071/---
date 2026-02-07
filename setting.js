26a3bad9fcf874c810b46d873f09c5e26fde61b7
Dani72071 <ranamuzammal145@gmail.com>
2026-02-07 19:23:34 +0500

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: ' ٠𓆩֓⤹• 𝑫𝑨𝑵𝑰𝑰-𝑴𝑫•⤸֓𓆪٠
+  author: '‎',
+  botName:٠𓆩֓⤹• 𝑫𝑨𝑵𝑰𝑰-𝑴𝑫•⤸֓𓆪٠
+  botOwner: 'SILVER', // Your name
+  ownerNumber: '923221859198', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.6",
+  updateZipUrl: "https://github.com/Dani72071/---",
+};
+
+module.exports = settings;
